eb7294aea0de698998b627ab04d75364843fce02
Андрей Малышев <andrei.malyshev@kodep.ru>
2012-08-29 02:59:56 +0400

    noConflict is not needed

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,8 +3,6 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-jQuery.noConflict();
-
 CKEDITOR.editorConfig = function( config )
 {
   // Define changes to default configuration here. For example:
